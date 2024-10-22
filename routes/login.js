bd02d278b7aa7a557855e73c642fa7429abb2d37
Wongsik <jingheidubz@gmail.com>
2024-10-21 17:41:58 -0700

    code does not work (temp)

--- routes/login.js
+++ routes/login.js
@@ -1,13 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('login', {title: "login"});
-})
 router.get('/login', (req, res) => {
     res.render('login', {title: "login"});
 })
-router.get('/register', (req, res) => {
+router.get('/', (req, res) => {
     res.render('register', {title: "login"});
 })
 router.get('/home', (req, res) => {
--- users.js
+++ users.js
@@ -1,13 +1,26 @@
-var car = require("car");
-var passportLocalCar = require("passport-local-car");
+// var car = require("car");
+// var passportLocalCar = require("passport-local-car");
 
-var UserSchema = new car.Schema({
-  username: String,
-  password: String,
-  email: String,
-  isAdmin: {type: Boolean, default: false}
-});
+// var UserSchema = new car.Schema({
+//   username: String,
+//   password: String,
+//   email: String,
+//   isAdmin: {type: Boolean, default: false}
+// });
 
-UserSchema.plugin(passportLocalCar)
+// UserSchema.plugin(passportLocalCar)
 
-module.exports = car.model("User", UserSchema);
+// module.exports = car.model("User", UserSchema);
+
+const request = require('request');
+
+var model = 'camry'
+request.get({
+  url: 'https://api.api-ninjas.com/v1/cars?model=' + model,
+  headers: {
+    'X-Api-Key': 'yiR+VnnJGsxdG7Tc8bsQ1A==6KNGDh3zIXneFKSd'
+  },
+}, function(error, response, body) {
+  if(error) return console.error('Request failed:', error);
+  else if(response.statusCode != 200) return console.error('Error:', response.statusCode, body.toString('utf8'));
+  else console.log(body) })
