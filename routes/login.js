6aca6f8a158054245ebbbfc73c42b2266d6eda8f
hanool0 <aronthechampion2435@gmail.com>
2024-10-21 18:03:27 -0700

    got rid of script on login.ejs

--- routes/login.js
+++ routes/login.js
@@ -7,8 +7,8 @@ router.get('/', (req, res) => {
 router.get('/login', (req, res) => {
     res.render('login', {title: "login"});
 })
-router.get('/register', (req, res) => {
-    res.render('register', {title: "login"});
+router.get('/signup', (req, res) => {
+    res.render('signup', {title: "login"});
 })
 router.get('/home', (req, res) => {
     res.render('home', {title: "login"});
